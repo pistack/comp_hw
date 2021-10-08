b34386389196d82c14c94f361126add093fd8154
pistack <pistatex@yonsei.ac.kr>
2021-10-08 18:18:58 +0900

    revise cpp code for HW1

--- HW1.cpp
+++ HW1.cpp
@@ -4,6 +4,7 @@ C++ code for homework1 in computer1 class in Yonsei University
 Using explicit Euler Method to solve Kepler problem
 */
 
+#include <cmath>
 #include <tuple>
 #include <vector>
 #include <iostream>
@@ -14,13 +15,17 @@ tuple<vector<double>, vector<double>> HW1(double t0, double t1,
 int n, double y0, double y0p) { 
 
     // initialize the variable
-    vector<double> t(n, 0);
-    vector<double> y(n, 0);
+    double spacing;
+    double y_inv;
+    vector<double> t(n+1, 0);
+    vector<double> y(n+1, 0);
+
+    spacing = (t1 - t0) / n;
 
     // use uniform n points bewteen t0 and t1
     // for time
-    for(int i=0; i< n; i++) {
-        t[i] = (t1-t0)/n*i;
+    for(int i=0; i< n+1; i++) {
+        t[i] = i*spacing;
     }
 
     // initial condition
@@ -30,8 +35,9 @@ int n, double y0, double y0p) {
     // Solve 2nd order ODE using
     // Explict Euler Method
 
-    for(int i = 0; i < n-2; i++) {
-        y[i+2] = ((t1-t0)/n)**2*(1/y[i]**3 - 1/y[i]**2) + \
+    for(int i = 0; i < n-1; i++) {
+        y_inv = 1 / y[i];
+        y[i+2] = pow(spacing, 2)*(pow(y_inv, 3) - pow(y_inv, 2)) + \
         2*y[i+1] - y[i];
     }
 
@@ -47,7 +53,7 @@ int main(void) {
     vector<double> t, y;
     tie(t, y) = HW1(t0, t1, n, y0, y0p);
 
-    for(int i = 0, i< n; i++) {
+    for (int i = 0; i < n+1; i++) {
         cout << t[i] << ',' << y[i] << endl;
     }
 
