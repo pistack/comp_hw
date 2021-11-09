422f6f83304acb3c51c0dc551a885966cc636845
pistack <pistatex@yonsei.ac.kr>
2021-11-09 16:24:05 +0900

    HW3: add details

--- HW3/result/script/plot_monitor_float.py
+++ HW3/result/script/plot_monitor_float.py
@@ -3,6 +3,9 @@ import matplotlib
 import matplotlib.pyplot as plt
 matplotlib.rcParams['text.usetex'] = True
 
+def exp_decay(s, l, s_min):
+    return l*np.exp(-l*(s-s_min))
+
 SMALL_SIZE = 10
 MEDIUM_SIZE = 15
 BIGGER_SIZE = 20
@@ -31,6 +34,35 @@ converged_dist2 = monitor2_reshape[burn:, 0]
 converged_dist3 = monitor3_reshape[burn:, 0]
 converged_dist4 = monitor4_reshape[burn:, 0]
 converged_dist5 = monitor5_reshape[burn:, 0]
+prob1, action1 = np.histogram(converged_dist, bins=1000)
+prob2, action2 = np.histogram(converged_dist2, bins=1000)
+prob3, action3 = np.histogram(converged_dist3, bins=1000)
+prob4, action4 = np.histogram(converged_dist4, bins=1000)
+prob5, action5 = np.histogram(converged_dist5, bins=1000)
+s1 = (action1[:1000]+action1[1:])/2
+s2 = (action2[:1000]+action2[1:])/2
+s3 = (action3[:1000]+action3[1:])/2
+s4 = (action4[:1000]+action4[1:])/2
+s5 = (action5[:1000]+action5[1:])/2
+
+print(s1[np.argmax(prob1)])
+print(s1[1]-s1[0])
+print(s2[np.argmax(prob2)])
+print(s2[1]-s2[0])
+print(s3[np.argmax(prob3)])
+print(s3[1]-s3[0])
+print(s4[np.argmax(prob4)])
+print(s4[1]-s4[0])
+print(s5[np.argmax(prob5)])
+print(s5[1]-s5[0])
+
+print(prob1[0]/np.max(prob1))
+print(prob2[0]/np.max(prob2))
+print(prob3[0]/np.max(prob3))
+print(prob4[0]/np.max(prob4))
+print(prob5[0]/np.max(prob5))
+
+'''
 plt.figure(1)
 plt.plot(np.arange(1, monitor1_reshape.shape[0]+1, 1), monitor1_reshape[:, 0], marker='o', 
 mfc='none', linestyle='none', color='red')
@@ -86,7 +118,7 @@ plt.xlabel('Action')
 plt.ylabel('Occurance')
 plt.legend()
 plt.savefig('plot_distf.png', dpi=100)
-plt.savefig('plot_distf_latex.png', dpi=300)
+plt.savefig('plot_distf.eps', dpi=300)
 plt.figure(7)
 plt.hist(converged_dist4, bins = 1000, label=r'$n_f=3$, setup: 2')
 plt.hist(converged_dist5, bins = 1000, label=r'$n_f=4$, setup: 2')
@@ -94,4 +126,5 @@ plt.xlabel('Action')
 plt.ylabel('Occurance')
 plt.legend()
 plt.savefig('plot_dist2f.png', dpi=100)
-plt.savefig('plot_dist2f_latex.png', dpi=300)
+plt.savefig('plot_dist2f.eps', dpi=300)
+'''
